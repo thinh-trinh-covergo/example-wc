dcda077c96cb6f129f81596d553b9ce7d458527e
thinh-trinh-covergo <thinh.trinh@covergo.com>
2025-07-23 14:30:26 +0700

    Update index.js

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ class Example extends HTMLElement {
         <div id="inner-html">
             CoverGo Platform
         </div>
+        <button id="button">Send event to host</button>
         `
   }
 }
@@ -13,13 +14,20 @@ customElements.define("cgp-example", Example)
 
 const initialise = async (args) => {
   console.log("`cgp-example` is initialised with", args)
-  const { root } = args
+  const { eventMaager, root } = args
 
   const content = root.getElementById("inner-html");
   content.innerHTML = `
     <div>The plugin has been initialized with</div>
-    <div>${JSON.stringify(args, null, 2)}</div
+    <pre>${JSON.stringify(args, null, 2)}</pre>
   `
+
+  root.getElementById("button").addEventListener("click", () => {
+    eventManager.publish({
+      type: 'wc:message',
+      data: "Hello from WebComponent!"
+    })
+  })
 }
 
 const destroy = async () => {
